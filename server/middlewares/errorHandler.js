88ba5997df673d42b9e812ed813330ddd0b8e309
pepedeulloa <lucascounhagobarreiro@gmail.com>
2024-01-15 00:17:40 +0100

    Organized scraping scripts into a dedicated folder and performed file refactoring

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,11 +1,15 @@
 export const errorHandler = (err, req, res, next) => {
 
-	if (err.name === 'IdentifierNotValidError') return res.status(400).json({
-		error: 'Identificador non válido'
-	});
-	else if (err.name === 'ResourceNotFoundError') return res.status(404).json({
-		error: 'Non se atopou o vinilo.'
-	});
-	else return res.status(500).json({ error: 'Erro no servidor.' });
+	if (err.name === 'IdentifierNotValidError')
+		return res.status(400).json({
+			error: 'Identificador non válido'
+		});
+	else if (err.name === 'ResourceNotFoundError')
+		return res.status(404).json({
+			error: 'Non se atopou o vinilo.'
+		});
+	else if (err.name !== 'IdentifierNotValidError' || err.name !== 'ResourceNotFoundError')
+		return res.status(500).json({ error: 'Erro no servidor.' });
 
+	next();
 }; 
