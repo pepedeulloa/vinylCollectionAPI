1119b87c7f6ca417e82e32b4343fd151e0d883a2
pepedeulloa <lucascounhagobarreiro@gmail.com>
2024-01-16 18:55:31 +0100

    Documented the new 'covers/:id' route using Swagger comments for enhanced documentation.

--- server/routes/Cover.routes.js
+++ server/routes/Cover.routes.js
@@ -3,6 +3,37 @@ import { getCoversById } from '../service/cover.js';
 
 const coversRouter = express.Router();
 
+/**
+	* @swagger
+	* tags:
+	*   name: Covers
+	*   description: API for managing records collection
+	* /api/covers/{id}:
+	*   get:
+	*     summary: Retrieve the covers of a record by ID
+	*     tags: [Covers]
+	*     parameters:
+	*       - in: path
+	*         name: id
+	*         required: true
+	*         schema:
+	*           type: integer
+	*         description: ID of the record of the covers to retrieve.
+	*     responses:
+	*       '200':
+	*         description: Successful response
+	*         content:
+	*           application/json:
+	*             example:
+	*               message: All covers of a record.
+	*       '500':
+	*         description: Server error
+	*         content:
+	*           application/json:
+	*             example:
+	*               error: Some server error
+	*/
+
 coversRouter.get('/:id', async (req, res, next) => {
 	try {
 		const covers = await getCoversById(req.params.id);
